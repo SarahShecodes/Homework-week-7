9f67c72ea3c6009cdde6e6adc1638c2f4e9bab50
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-02-28 15:58:44 +0000

    function display city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  ////
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "46d856ad81bf24c97ff95fd0280e6d42";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=lisbon&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
