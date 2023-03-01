c640e280ae819516537f69c91cfdd34661521c63
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-03-01 15:29:00 +0000

    CSS link Fahrenheit and Celsius

--- src/index.js
+++ src/index.js
@@ -65,12 +65,16 @@ function displayFahrenheitTemperature(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
